5ccbc8faaae366b1e155ffccc8c732c6f7b4b0ab
xedthtrdj <fdghry>
2025-11-01 10:24:51 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import TopBar from './Components/TopBar'
 import MainBox from './Pages/MainBox'
+import CreateBy from './Components/CreatedBy'
 
 export default function App() {
   return (
-    <div>
+    <div className='relative'>
+      <CreateBy/>
       <TopBar/>
       <MainBox/>
     </div>
---
+++ src/Components/CreatedBy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CreatedBy() {
+  return (
+    <div title='Creatad By' className="z-99 fixed right-5 bottom-5 w-12 h-12 bg-amber-300 rounded-full overflow-hidden opacity-65 cursor-pointer hover:opacity-100 transition">
+        <a target="_blank"  href="https://rudro3132004.github.io/Portfolio">
+          <img  className='' src="https://rudro3132004.github.io/Portfolio/assets/image1-j3t-0lJ6.jpeg" alt="" />
+        </a>
+    </div>
+  )
+}
